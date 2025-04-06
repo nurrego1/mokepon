3d1e9c6efc5879426eef5de3ce5c5a66ad317a46
nurrego1 <nurrego1@outlook.com>
2025-04-06 10:14:32 -0500

    Clase 18 - iniciando el js del juego

---
+++ Mokepon/js/mokepon.js
@@ -0,0 +1,11 @@
+//Funcion seleccionar Mascota
+function seleccionarMascotaJugador() {
+    
+}
+
+//Variables
+let botonMascota = document.getElementById("boton-mascota");
+let botonFuego = document.getElementById("boton-fuego");        
+
+//Event listeners
+botonMascota.addEventListener("click",seleccionarMascotaJugador); //El primer elemento es el evento que va a escuchar, y el segundo es la accion o funcion que se va a ejecutar
